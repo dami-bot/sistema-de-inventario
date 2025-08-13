ace2343e0bb1f9f5f949d1507f54326f39ac2944
damian <damianl32cooking@gmail.com>
2025-08-13 20:30:47 -0300

    borrado de archivos

--- backend/prisma/migrations/20250725225832_add_purchase_history/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Purchase` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `items` JSON NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ComprasModule } from './compras/compras.module';
-import { ProductosController } from './productos/productos.controller';
-import { ProductosModule } from './productos/productos.module';
-@Module({
-  imports: [ComprasModule, ProductosModule],
-  controllers: [ProductosController],
-})
-export class AppModule { }
